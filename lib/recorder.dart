ba1fdbe090892036b15a93a2c9a7a14129f82657
suhas4090 <35102923+suhas4090@users.noreply.github.com>
2018-07-27 11:17:59 +0530

    mic recorder and audio player

--- lib/recorder.dart
+++ lib/recorder.dart
@@ -1,77 +1,82 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
 import 'package:audio_recorder/audio_recorder.dart';
+import 'package:audioplayer/audioplayer.dart';
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
+typedef void OnError(Exception exception);
 
-class _MyAppState extends State<MyApp> {
+enum PlayerState { stopped, playing, paused }
 
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Plugin audio recorder'),
-        ),
-        body: new AppBody(),
-      ),
-    );
-  }
-}
-
-class AppBody extends StatefulWidget{
+class Recorder extends StatefulWidget{
 
   @override
-  State<StatefulWidget> createState() => new AppBodyState();
+  State<StatefulWidget> createState() => new RecorderState();
 }
 
-class AppBodyState extends State<AppBody>{
+class RecorderState extends State<Recorder>{
   Recording _recording = new Recording();
+  AudioPlayer audioPlayer;
+  var btnicon = Icons.mic;
+  var btnclr = Colors.white;
+  var playMusic = Icons.play_arrow, playMusicColor = Colors.white;
+  bool _isRecorded = false;
   bool _isRecording = false;
-  Random random = new Random();
-  TextEditingController _controller = new TextEditingController();
+  Duration duration;
+  String filePath = "";
+
+  PlayerState playerState = PlayerState.stopped;
+  get isPlaying => playerState == PlayerState.playing;
+  get isPaused => playerState == PlayerState.paused;
+
+  @override
+  void initState() {
+    super.initState();
+    _initAudioPlayer();
+  }
+
+  void _initAudioPlayer() {
+    audioPlayer = new AudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Padding(
-        padding: new EdgeInsets.all(8.0),
-        child: new Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              new FlatButton(onPressed: _isRecording ? null : _start, child: new Text("Start"), color: Colors.green,),
-              new FlatButton(onPressed: _isRecording ? _stop : null, child: new Text("Stop"), color: Colors.red,),
-              new TextField(controller: _controller, decoration: new InputDecoration(hintText: 'Enter a custom path',),),
-              new Text("File path of the record: ${_recording.path}"),
-              new Text("Format: ${_recording.audioOutputFormat}"),
-              new Text("Extension : ${_recording.extension}"),
-              new Text("Audio recording duration : ${_recording.duration.toString()}" )
-            ]),
-      ),
+    return Container(
+      margin: new EdgeInsets.all(20.0),
+          child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+        FloatingActionButton(onPressed: _isRecorded ?  _playaudio : _stopaudio, child: Icon(playMusic, color: playMusicColor,), ),
+        FloatingActionButton(onPressed: _isRecording ? _stop : _start, child: Icon(btnicon, color: btnclr,), ),
+          ]),
     );
   }
 
   _start() async {
     try {
       if (await AudioRecorder.hasPermissions) {
-        if (_controller.text != null && _controller.text != "") {
-          String path = _controller.text;
-          if (!_controller.text.contains('/')) {
-            Directory appDocDirectory = await getExternalStorageDirectory();
-            path = appDocDirectory.path + '/' + _controller.text;
+          String path = "recorder.m4a";
+          Directory appDocDirectory = await getExternalStorageDirectory();
+          String filepath = appDocDirectory.path + '/' + path;
+          print("Start recording: $filepath");
+          File file = new File(filepath);
+          if(file.exists() != null){
+            print("Deleted the fie in the path");
+              file.delete();
           }
-          print("Start recording: $path");
-          await AudioRecorder.start(path: path, audioOutputFormat: AudioOutputFormat.AAC);
-        } else {
-          await AudioRecorder.start();
-        }
+          await AudioRecorder.start(path: filepath, audioOutputFormat: AudioOutputFormat.AAC);
+        
         bool isRecording = await AudioRecorder.isRecording;
         setState(() {
+          _isRecorded = false;
+          btnicon = Icons.stop;
+          btnclr = Colors.red;
           _recording = new Recording(duration: new Duration(), path: "");
           _isRecording = isRecording;
         });
@@ -90,9 +95,37 @@ class AppBodyState extends State<AppBody>{
     File file = new File(recording.path);
     print("  File length: ${await file.length()}");
     setState(() {
+      _isRecorded = true;
+      btnicon = Icons.mic;
+      btnclr = Colors.white;
       _recording = recording;
       _isRecording = isRecording;
+      duration = recording.duration;
     });
-    _controller.text = recording.path;
+    filePath = recording.path;
+  }
+
+  Future _playaudio() async {
+    await audioPlayer.play(filePath, isLocal: true);
+    setState(() {
+      _isRecorded = false;
+      playMusicColor = Colors.red;
+      playMusic = Icons.stop;
+      playerState = PlayerState.playing;
+      });
+      new Future.delayed(duration, () {
+              _stopaudio();
+            });
+  }
+
+  Future _stopaudio() async {
+    await audioPlayer.stop();
+    print("Music is stopped and finished");
+    setState(() {
+      _isRecorded = true;
+      playMusicColor = Colors.white;
+      playMusic = Icons.play_arrow;
+      playerState = PlayerState.stopped;
+    });
   }
 }
