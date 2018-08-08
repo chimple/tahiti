7bd84f61e505865b00f0777eec95756d39f65970
suhas4090 <35102923+suhas4090@users.noreply.github.com>
2018-08-08 15:53:35 +0530

    formated

--- lib/edit_text_view.dart
+++ lib/edit_text_view.dart
@@ -8,23 +8,25 @@ class EditTextView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return fontType != null ? Container(
-      height: 200.0,
-      child: Center(
-          child: Padding(
-        child: Center(
-            child: TextField(
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 30.0,
-                    color: const Color(0xFF000000),
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                    fontFamily: fontType),
-                decoration:
-                    new InputDecoration.collapsed(hintText: 'Type Here'))),
-        padding: EdgeInsets.all(20.0),
-      )),
-    ): Text(" ");
+    return fontType != null
+        ? Container(
+            height: 200.0,
+            child: Center(
+                child: Padding(
+              child: Center(
+                  child: TextField(
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          color: const Color(0xFF000000),
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          fontFamily: fontType),
+                      decoration: new InputDecoration.collapsed(
+                          hintText: 'Type Here'))),
+              padding: EdgeInsets.all(20.0),
+            )),
+          )
+        : Text(" ");
   }
 }
