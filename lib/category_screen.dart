803a66f7dd1b3b9b9a03cc01ffec6e008ba50acd
SATISH <36834824+satish754ss@users.noreply.github.com>
2018-09-27 19:53:03 +0530

    Updated Category Screen.

--- lib/category_screen.dart
+++ lib/category_screen.dart
@@ -46,125 +46,120 @@ class CategoryScreenState extends State<CategoryScreen> {
     });
   }
 
-  bool overlayShouldBeVisible = true;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    if (overlayShouldBeVisible)
-      return Material(
-        color: Colors.black87,
-        key: Key('overLayVisibleContainer'),
-        child: SizedBox(
-          height: size.height,
-          width: size.width,
-          child: Stack(
-            overflow: Overflow.visible,
-            fit: StackFit.loose,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Padding(
-                      padding:
-                          EdgeInsets.only(top: 10.0, left: 15.0, right: 15.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(''),
-                          Text('Sticker',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold)),
-                          IconButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon: Icon(
-                              Icons.done,
-                              color: Colors.white,
-                              size: 40.0,
-                            ),
-                          )
-                        ],
-                      ),
+    return Material(
+      color: Colors.black87,
+      key: Key('overLayVisibleContainer'),
+      child: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Stack(
+          overflow: Overflow.visible,
+          fit: StackFit.loose,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding:
+                        EdgeInsets.only(top: 10.0, left: 15.0, right: 15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(''),
+                        Text('Sticker',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold)),
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(
+                            Icons.done,
+                            color: Colors.white,
+                            size: 40.0,
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  Container(
-                    height: 3.0,
-                    color: Colors.white,
-                  ),
-                  Expanded(
-                      flex: 1,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: _itemRange
-                            .map((e) => Container(
-                                  color: e.item1 == highlightedItem
-                                      ? Color(0XFFF4F4F4)
-                                      : Colors.transparent,
-                                  child: InkWell(
-                                      onTap: () {
-                                        final offset = (_scrollController
-                                                    .position.maxScrollExtent +
-                                                _scrollController.position
-                                                    .viewportDimension) *
-                                            e.item2 /
-                                            _itemCount;
-                                        _scrollController.jumpTo(offset);
-                                      },
-                                      child: buildIndexItem(context, e.item1,
-                                          e.item1 == highlightedItem)),
-                                ))
-                            .toList(growable: false),
-                      )),
-                  Container(
-                    height: 3.0,
-                    color: Colors.white,
-                  ),
-                  Expanded(
-                      flex: 7,
-                      child: Container(
-                        child: CustomScrollView(
-                            controller: _scrollController,
-                            scrollDirection: Axis.vertical,
-                            slivers: widget.items.keys
-                                .map((e) => SliverGrid(
-                                      gridDelegate:
-                                          SliverGridDelegateWithFixedCrossAxisCount(
-                                              crossAxisCount:
-                                                  widget.itemCrossAxisCount),
-                                      delegate: SliverChildBuilderDelegate(
-                                          (BuildContext context, int index) {
-                                        return InkWell(
-                                            onTap: () {
-                                              addSticker(
-                                                  widget.items[e][index]);
-                                              Navigator.pop(context);
-                                            },
-                                            child: Container(
-                                              child: buildItem(context,
-                                                  widget.items[e][index], true),
-                                            ));
-                                      }, childCount: widget.items[e].length),
-                                    ))
-                                .toList(growable: false)),
-                      )),
-                  Expanded(
-                    child: ColorPicker(),
-                    flex: 2,
-                  ),
-                ],
-              )
-            ],
-          ),
+                ),
+                Container(
+                  height: 3.0,
+                  color: Colors.white,
+                ),
+                Expanded(
+                    flex: 1,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: _itemRange
+                          .map((e) => Container(
+                                color: e.item1 == highlightedItem
+                                    ? Color(0XFFF4F4F4)
+                                    : Colors.transparent,
+                                child: InkWell(
+                                    onTap: () {
+                                      final offset = (_scrollController
+                                                  .position.maxScrollExtent +
+                                              _scrollController
+                                                  .position.viewportDimension) *
+                                          e.item2 /
+                                          _itemCount;
+                                      _scrollController.jumpTo(offset);
+                                    },
+                                    child: buildIndexItem(context, e.item1,
+                                        e.item1 == highlightedItem)),
+                              ))
+                          .toList(growable: false),
+                    )),
+                Container(
+                  height: 3.0,
+                  color: Colors.white,
+                ),
+                Expanded(
+                    flex: 7,
+                    child: Container(
+                      child: CustomScrollView(
+                          controller: _scrollController,
+                          scrollDirection: Axis.vertical,
+                          slivers: widget.items.keys
+                              .map((e) => SliverGrid(
+                                    gridDelegate:
+                                        SliverGridDelegateWithFixedCrossAxisCount(
+                                            crossAxisCount:
+                                                widget.itemCrossAxisCount),
+                                    delegate: SliverChildBuilderDelegate(
+                                        (BuildContext context, int index) {
+                                      return InkWell(
+                                          onTap: () {
+                                            addSticker(widget.items[e][index]);
+                                            Navigator.pop(context);
+                                          },
+                                          child: Container(
+                                            child: buildItem(context,
+                                                widget.items[e][index], true),
+                                          ));
+                                    }, childCount: widget.items[e].length),
+                                  ))
+                              .toList(growable: false)),
+                    )),
+                Expanded(
+                  child: ColorPicker(),
+                  flex: 2,
+                ),
+              ],
+            )
+          ],
         ),
-      );
-    else
-      return Container();
+      ),
+    );
   }
 
   Widget buildItem(BuildContext context, Iconf conf, bool enabled) {
