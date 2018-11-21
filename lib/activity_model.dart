ae6ade362f1bfc7e6ff06aff3d90b50ad63329e5
ramaranjandey <36834875+ramaranjandey@users.noreply.github.com>
2018-11-21 15:58:48 +0530

    code updated for dotsketch

--- lib/activity_model.dart
+++ lib/activity_model.dart
@@ -140,6 +140,7 @@ class ActivityModel extends Model {
   bool get isDotSketch => _isdotSketch;
   set isDotSketch(bool t) {
     _isdotSketch = t;
+    _isDrawing = false;
     notifyListeners();
   }
 
